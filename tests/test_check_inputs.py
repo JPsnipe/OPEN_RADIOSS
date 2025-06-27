fbd5a8c74dc8711130facdefa615706f08402877
JPsnipe <javiportodlc@gmail.com>
2025-06-27 23:44:20 +0200

    Add SOLID property compatibility checks

--- tests/test_check_inputs.py
+++ tests/test_check_inputs.py
@@ -12,3 +12,48 @@ def test_check_rad_inputs_basic():
         parts=[{"id": 1, "pid": 1, "mid": 1}],
     )
     assert all(ok for ok, _ in res)
+
+
+def test_check_rad_inputs_solid_conflict():
+    res = utils.check_rad_inputs(
+        use_cdb_mats=True,
+        materials={1: {"LAW": "LAW1"}},
+        use_impact=False,
+        impact_materials=None,
+        bcs=None,
+        interfaces=None,
+        properties=[
+            {
+                "id": 1,
+                "type": "SOLID",
+                "Isolid": 24,
+                "h": 0.1,
+            }
+        ],
+        parts=[{"id": 1, "pid": 1, "mid": 1}],
+        advanced=True,
+    )
+    assert any(not ok for ok, _ in res)
+
+
+def test_check_rad_inputs_solid_ok():
+    res = utils.check_rad_inputs(
+        use_cdb_mats=True,
+        materials={1: {"LAW": "LAW1"}},
+        use_impact=False,
+        impact_materials=None,
+        bcs=None,
+        interfaces=None,
+        properties=[
+            {
+                "id": 1,
+                "type": "SOLID",
+                "Isolid": 24,
+                "Iframe": 2,
+                "dn": 0.1,
+            }
+        ],
+        parts=[{"id": 1, "pid": 1, "mid": 1}],
+        advanced=True,
+    )
+    assert all(ok for ok, _ in res)
